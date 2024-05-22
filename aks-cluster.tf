19fa5fcdfeb3f4f0ce8663f236d610020a529323
Ranu Jain <65921042+RanuJain131@users.noreply.github.com>
2024-05-23 02:51:46 +0530

    Update aks-cluster.tf

--- aks-cluster.tf
+++ aks-cluster.tf
@@ -5,6 +5,11 @@ resource "random_pet" "prefix" {}
 
 provider "azurerm" {
   features {}
+  subscription_id            = "c61ea177-3092-49b2-b416-2742300b59fd"
+  client_id                  = "d2eb23fb-162a-4198-87c8-16e573d59205"
+  client_secret              = "1575bd5f-c65a-47c3-8ff6-1d7c90c94e90"
+  tenant_id                  = "d7d3f2c0-a6ee-4480-8526-ea05647335b3
+  skip_provider_registration = true
 }
 
 resource "azurerm_resource_group" "default" {
