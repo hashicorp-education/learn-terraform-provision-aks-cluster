ba2135e4dccfe77168d9ab1936dc769289bec9a5
Brian McClain <brianmmcclain@gmail.com>
2025-11-20 11:01:45 -0500

    Move provider block to providers.tf

--- aks-cluster.tf
+++ aks-cluster.tf
@@ -3,10 +3,6 @@
 
 resource "random_pet" "prefix" {}
 
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_resource_group" "default" {
   name     = "${random_pet.prefix.id}-rg"
   location = "West US 2"
